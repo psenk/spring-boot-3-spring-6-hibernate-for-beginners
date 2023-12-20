ffc294650605ca4b9aca0d8a0198a3d8f4c461a8
Paul Senkbeil <psenk168@gmail.com>
2023-12-20 16:56:16 -0600

    testing application properties and custom properties

--- springbootdemo/src/main/java/com/luv2code/workingcopy/springbootdemo/rest/FunRestController.java
+++ springbootdemo/src/main/java/com/luv2code/workingcopy/springbootdemo/rest/FunRestController.java
@@ -1,10 +1,23 @@
 package com.luv2code.workingcopy.springbootdemo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // injecting custom properties from application.properties
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    // new endpoint for /teaminfo
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
 
     // expose "/" (root), say Hello World
 
@@ -12,4 +25,5 @@ public class FunRestController {
     public String sayHello() {
         return "Hello World!";
     }
+
 }
