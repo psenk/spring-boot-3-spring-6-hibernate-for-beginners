ee08308a79d2b118c21de734cd2d7c9cd302b812
Paul Senkbeil <psenk168@gmail.com>
2023-12-27 19:11:56 -0600

    Testing roles security on custom tables

--- springbootdemo/src/main/java/com/luv2code/workingcopy/springbootdemo/security/DemoSecurityConfig.java
+++ springbootdemo/src/main/java/com/luv2code/workingcopy/springbootdemo/security/DemoSecurityConfig.java
@@ -1,53 +1,50 @@
 package com.luv2code.workingcopy.springbootdemo.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+// {xxxx} before a password = specify encryption (noop, bcrypt, etc)
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // JDBC support, connects to db specified in application.properties
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
 
-        UserDetails john = User.builder()
-            .username("john")
-            .password("{noop}test123")
-            .roles("EMPLOYEE")
-            .build();
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
-        UserDetails mary = User.builder()
-            .username("mary")
-            .password("{noop}test123")
-            .roles("EMPLOYEE", "MANAGER")
-            .build();
+        // two custom methods REQUIRED if using custom tables w/ jdbc
+        // define query to retrieve user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery("SELECT user_id, pw, active FROM members WHERE user_id=?");
 
-        UserDetails susan = User.builder()
-            .username("susan")
-            .password("{noop}test123")
-            .roles("EMPLOYEE", "MANAGER", "ADMIN")
-            .build();
+        // define query to retrieve the roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("SELECT user_id, role FROM roles WHERE user_id=?");
 
-        return new InMemoryUserDetailsManager(john, mary, susan);
+        return jdbcUserDetailsManager;
     }
 
+    // filter http method requests based on end point and roles
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests(configurer -> 
-            configurer
-            .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
-            .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
-            .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-            .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
-            .requestMatchers(HttpMethod.DELETE, "/api/employees").hasRole("ADMIN")
-            );
+        http.authorizeHttpRequests(configurer -> configurer
+                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
 
         // use basic http authentication
         http.httpBasic(Customizer.withDefaults());
@@ -58,5 +55,32 @@ public class DemoSecurityConfig {
 
         return http.build();
     }
+
+    // hard coded users in memory for testing
+    /*
+     * @Bean
+     * public InMemoryUserDetailsManager userDetailsManager() {
+     * 
+     * UserDetails john = User.builder()
+     * .username("john")
+     * .password("{noop}test123")
+     * .roles("EMPLOYEE")
+     * .build();
+     * 
+     * UserDetails mary = User.builder()
+     * .username("mary")
+     * .password("{noop}test123")
+     * .roles("EMPLOYEE", "MANAGER")
+     * .build();
+     * 
+     * UserDetails susan = User.builder()
+     * .username("susan")
+     * .password("{noop}test123")
+     * .roles("EMPLOYEE", "MANAGER", "ADMIN")
+     * .build();
+     * 
+     * return new InMemoryUserDetailsManager(john, mary, susan);
+     * }
+     */
 
 }
