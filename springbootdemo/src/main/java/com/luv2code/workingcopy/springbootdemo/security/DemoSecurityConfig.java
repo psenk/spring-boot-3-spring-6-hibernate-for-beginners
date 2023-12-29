053a1138b6366a98f03206ad47f146a253b70d8e
Tyler <psenk168@gmail.com>
2023-12-28 19:26:37 -0600

    Fixed PUT issue w/ Swabhelp

--- springbootdemo/src/main/java/com/luv2code/workingcopy/springbootdemo/security/DemoSecurityConfig.java
+++ springbootdemo/src/main/java/com/luv2code/workingcopy/springbootdemo/security/DemoSecurityConfig.java
@@ -43,7 +43,7 @@ public class DemoSecurityConfig {
                 .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
                 .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
                 .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/employees/**").hasRole("MANAGER")
                 .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
 
         // use basic http authentication
