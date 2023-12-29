9993116e2ddd90723aef56dbde9bebe8d2ced74b
Tyler <psenk168@gmail.com>
2023-12-29 17:43:00 -0600

    Testing radio buttons with Thymeleaf.

--- spring-mvc-demo/src/main/java/home/springmvcdemo/model/Student.java
+++ spring-mvc-demo/src/main/java/home/springmvcdemo/model/Student.java
@@ -6,6 +6,7 @@ public class Student {
     private String firstName;
     private String lastName;
     private String country;
+    private String favoriteLanguage;
 
     // constructors
     public Student() {
@@ -35,4 +36,13 @@ public class Student {
     public void setCountry(String country) {
         this.country = country;
     }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
+    }
+
 }
