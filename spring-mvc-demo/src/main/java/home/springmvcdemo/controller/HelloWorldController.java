40c82ced745c6b6056047d0de947a40359ababa6
Tyler <psenk168@gmail.com>
2023-12-29 14:07:43 -0600

    Demoing request parameters and adding data to model

--- spring-mvc-demo/src/main/java/home/springmvcdemo/controller/HelloWorldController.java
+++ spring-mvc-demo/src/main/java/home/springmvcdemo/controller/HelloWorldController.java
@@ -1,7 +1,11 @@
 package home.springmvcdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
@@ -18,4 +22,32 @@ public class HelloWorldController {
         return "helloworld";
     }
 
+    // method to read form data
+    // add data to model
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model){
+
+        // read request from html form
+        String name = request.getParameter("studentName");
+
+        // convert to uppercase
+        name = name.toUpperCase();
+
+        // create message
+        String result = "Yo! " + name;
+
+        // add message to model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String name, Model model) {
+        name = name.toUpperCase();
+        String result = "Hey my Friend from v3! " + name;
+        model.addAttribute("message", result);
+        return "helloworld";
+    }
+    
 }
