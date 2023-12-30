147457a83c440beee8ee18627175087b682c32b3
Tyler <psenk168@gmail.com>
2023-12-29 20:41:06 -0600

    Testing min/max validation

--- spring-mvc-demo/src/main/java/home/springmvcdemo/controller/CustomerController.java
+++ spring-mvc-demo/src/main/java/home/springmvcdemo/controller/CustomerController.java
@@ -1,9 +1,12 @@
 package home.springmvcdemo.controller;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -12,6 +15,16 @@ import jakarta.validation.Valid;
 
 @Controller
 public class CustomerController {
+
+    // INIT BINDER - this is done to the data before it is mapped
+    @InitBinder
+    public void initbinder(WebDataBinder dataBinder) {
+        
+        // trims leading and trailing whitespace
+        // returns null if all whitespace
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model model) {
@@ -26,6 +39,8 @@ public class CustomerController {
     // BindingResult stores the results of the above validation (if data passed validation or not)
     public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult result) {
         
+        System.out.println("Last name: |" + customer.getLastName() + "|");
+
         if (result.hasErrors()) {
             return "customer-form";
         }
--- spring-mvc-demo/src/main/java/home/springmvcdemo/model/Customer.java
+++ spring-mvc-demo/src/main/java/home/springmvcdemo/model/Customer.java
@@ -1,5 +1,7 @@
 package home.springmvcdemo.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -12,18 +14,32 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName;
 
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to ten")
+    private int freePasses;
 
     // getters and setters
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
     }
 }
