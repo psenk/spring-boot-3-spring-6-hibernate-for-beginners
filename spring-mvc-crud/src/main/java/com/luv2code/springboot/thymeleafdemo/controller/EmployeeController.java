941a89c2eeb01b64b53742e4e59050476af16763
Tyler <psenk168@gmail.com>
2023-12-31 21:17:58 -0600

    Added Update and Delete buttons to the employee directory page

--- spring-mvc-crud/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ spring-mvc-crud/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
@@ -20,6 +23,7 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
+    // showing employee directory, default view
     @GetMapping("/list")
     public String listEmployees(Model model) {
         
@@ -27,7 +31,47 @@ public class EmployeeController {
 
         model.addAttribute("employees", employees);
 
-        return "list-employees";
+        return "employees/list-employees";
     }
 
+    // getting the form to add a new employee to the database
+    @GetMapping("/addEmployeeForm")
+    public String addEmployeeForm(Model model) {
+        
+        Employee newEmployee = new Employee();
+
+        model.addAttribute("employee", newEmployee);
+
+        return "employees/employee-form";
+    }
+
+    // saving the new employee to the database
+    @PostMapping("/save")
+    // getting the employee from the model to save
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+
+        employeeService.save(employee);
+
+        return "redirect:/employees/list";
+    }
+
+    // getting employee from the database for updating, by id number
+    @GetMapping("/updateEmployeeForm")
+    public String updateEmployeeForm(@RequestParam("employeeId") int id, Model model) {
+
+        // adding the employee to the model for form access
+        Employee employee = employeeService.findById(id);
+        model.addAttribute("employee", employee);
+
+        return "employees/employee-form";
+    }
+
+    // deleting employee from the database, by id number
+    @GetMapping("/deleteEmployee")
+    public String deleteEmployee(@RequestParam("employeeId") int id) {
+
+        employeeService.deleteById(id);
+
+        return "redirect:/employees/list";
+    }
 }
--- spring-mvc-crud/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ spring-mvc-crud/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.luv2code.springboot.thymeleafdemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
@@ -7,5 +9,8 @@ import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code LOL!
+
+    // spring magic, sorting employee table before returning
+    public List<Employee> findAllByOrderByLastNameAsc();
 
 }
