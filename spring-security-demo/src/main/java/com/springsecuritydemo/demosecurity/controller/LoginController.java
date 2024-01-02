8191f4e3d30598c048e070ed38d77aa4eb8e322a
Tyler <psenk168@gmail.com>
2024-01-01 18:58:53 -0600

    Created login form, http security filter, and login controller.

---
+++ spring-security-demo/src/main/java/com/springsecuritydemo/demosecurity/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.springsecuritydemo.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    
+    @GetMapping("/loginPage")
+    public String loginPage(Model model) {
+        return "plain-login";
+    }
+
+}
