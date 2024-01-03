bb7c7ee83a6f3d64e2636c579a7c3526fdb430db
Tyler <psenk168@gmail.com>
2024-01-02 18:19:26 -0600

    Removed hard coded users, pulling users from DB.

--- spring-security-demo/src/main/java/com/springsecuritydemo/demosecurity/security/DemoSecurityConfig.java
+++ spring-security-demo/src/main/java/com/springsecuritydemo/demosecurity/security/DemoSecurityConfig.java
@@ -1,26 +1,21 @@
 package com.springsecuritydemo.demosecurity.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // JDBC support
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-
-        UserDetails john = User.builder().username("john").password("{noop}test123").roles("EMPLOYEE").build();
-        UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE", "MANAGER")
-                .build();
-        UserDetails susan = User.builder().username("susan").password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN").build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     // filter incoming http requests
@@ -42,9 +37,28 @@ public class DemoSecurityConfig {
                 .logout(logout -> logout.permitAll())
                 // custom error page
                 .exceptionHandling(configurer -> configurer
-                .accessDeniedPage("/access-denied"));
+                        .accessDeniedPage("/access-denied"));
 
         return http.build();
     }
+
+    /*
+     * @Bean
+     * public InMemoryUserDetailsManager userDetailsManager() {
+     * 
+     * UserDetails john =
+     * User.builder().username("john").password("{noop}test123").roles("EMPLOYEE").
+     * build();
+     * UserDetails mary =
+     * User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE",
+     * "MANAGER")
+     * .build();
+     * UserDetails susan =
+     * User.builder().username("susan").password("{noop}test123")
+     * .roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+     * 
+     * return new InMemoryUserDetailsManager(john, mary, susan);
+     * }
+     */
 
 }
